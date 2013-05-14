42c1e2017221a47b12cfa1e011afab7b67a9cd84
Benjamin Liles <benliles@arch.tamu.edu>
2013-05-14 11:05:05 -0500

    Added admin for profiles

--- dprofiling/admin.py
+++ dprofiling/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
-from dprofiling.models import Session
+from dprofiling.models import Session, Profile
 
 
 
 class SessionAdmin(admin.ModelAdmin):
     pass
 
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Session, SessionAdmin)
+admin.site.register(Profile, ProfileAdmin)
 
